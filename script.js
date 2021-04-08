80ddd26fb719412aa6bc39be69a25feebce87d1e
May Pattaranan <suras.sunktong@gmail.com>
2021-04-08 23:38:23 +0200

    fix API, rename class/id complete

--- script.js
+++ script.js
@@ -7,20 +7,35 @@ const rateEl = document.getElementById("rate");
 const swap = document.getElementById("swap");
 
 // Fetch exchange rates and update the DOM
-async function calculate() {
+function calculate() {
   const currency_one = currencyEl_one.value;
   const currency_two = currencyEl_two.value;
 
-  const response = await fetch(
+  fetch(
     `https://v6.exchangerate-api.com/v6/d80a04782d274f5f245f7344/latest/${currency_one}`
-  );
-  const data = await response.json();
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      // console.log(data);
+      const rate = data.conversion_rates[currency_two];
+
+      rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
+
+      amountEl_two.value = (amountEl_one.value * rate).toFixed(2);
+    });
 }
 
-// Even Listeners
+// Event listeners
 currencyEl_one.addEventListener("change", calculate);
 amountEl_one.addEventListener("input", calculate);
 currencyEl_two.addEventListener("change", calculate);
 amountEl_two.addEventListener("input", calculate);
+
+swap.addEventListener("click", () => {
+  const temp = currencyEl_one.value;
+  currencyEl_one.value = currencyEl_two.value;
+  currencyEl_two.value = temp;
+  calculate();
+});
 
 calculate();
